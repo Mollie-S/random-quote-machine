307f285cf58fb726ed8db1d1837b1aab2efbb478
Mollie-S <olcha.olcha@gmail.com>
2020-06-02 18:22:04 +0200

    Added fetch(api) to get data with quotes.

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -2,6 +2,17 @@ import React from "react";
 import TwitterImg from "../images/Twitter_Social_Icon_Rounded_Square_Color.png";
 
 function Wrapper() {
+  // const proxy = "https://cors-anywhere.herokuapp.com/";
+  const api = "https://api.paperquotes.com/apiv1/quotes/?lang=en";
+
+  fetch(api, {
+    headers: {
+      Authorization: "Token{547e9ce1fd76c539001052061b1ec2b93c2b6aff}",
+    },
+  })
+    .then((request) => request.json())
+    .then((data) => console.log(data));
+
   return (
     <div id="quote-box">
       <h3 id="text">Text</h3>
