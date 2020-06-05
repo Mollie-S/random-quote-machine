62f2cf525d29de0d94c6c1c875539aede0ea69b2
Mollie-S <olcha.olcha@gmail.com>
2020-06-05 12:18:36 +0200

    Replaced quotes from external api with an array with movie quotes.

---
+++ src/assets/movie_quotes.js
@@ -0,0 +1,209 @@
+const movieQuotes = [
+  {
+    quotation: "May the Force be with you.",
+    author: "Star Wars,1977",
+  },
+  {
+    quotation: "There's no place like home.",
+    author: "The Wizard of Oz,1939",
+  },
+  {
+    quotation: "I'm the king of the world!",
+    author: "Titanic,1997",
+  },
+  {
+    quotation:
+      "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
+    author: "Dead Poets Society,1989",
+  },
+  {
+    quotation: "Elementary, my dear Watson.",
+    author: "The Adventures of Sherlock Holmes,1939",
+  },
+  {
+    quotation: "It's alive! It's alive!",
+    author: "Frankenstein,1931",
+  },
+  {
+    quotation:
+      "My mama always said life was like a box of chocolates. You never know what you're gonna get.",
+    author: "Forrest Gump,1994",
+  },
+  {
+    quotation: "I'll be back.",
+    author: "The Terminator,1984",
+  },
+  {
+    quotation: "You're gonna need a bigger boat.",
+    author: "Jaws,1975",
+  },
+  {
+    quotation: "Here's looking at you, kid.",
+    author: "Casablanca,1942",
+  },
+  {
+    quotation: "My precious.",
+    author: "The Lord of the Rings: Two Towers,2002",
+  },
+  {
+    quotation: "Houston, we have a problem.",
+    author: "Apollo13,1995",
+  },
+  {
+    quotation: "There's no crying in baseball!",
+    author: "A League of Their Own,1992",
+  },
+  {
+    quotation: "E.T. phone home.",
+    author: "E.T. the Extra-Terrestrial,1982",
+  },
+  {
+    quotation: "You can't handle the truth!",
+    author: "A Few Good Men,1992",
+  },
+  {
+    quotation: "A martini. Shaken, not stirred.",
+    author: "Goldfinger,1964",
+  },
+  {
+    quotation:
+      "Life is a banquet, and most poor suckers are starving to death!",
+    author: "Auntie Mame,1958",
+  },
+  {
+    quotation: "If you build it, he will come.",
+    author: "Field of Dreams,1989",
+  },
+  {
+    quotation: "The stuff that dreams are made of.",
+    author: "The Maltese Falcon,1941",
+  },
+  {
+    quotation: "Magic Mirror on the wall, who is the fairest one of all?",
+    author: "Snow White and the Seven Dwarfs,1937",
+  },
+  {
+    quotation: "Keep your friends close, but your enemies closer.",
+    author: "The Godfather Part II,1974",
+  },
+  {
+    quotation: "I am your father.",
+    author: "Star Wars Episode V: The Empire Strikes Back,1980",
+  },
+  {
+    quotation: "Just keep swimming.",
+    author: "Finding Nemo,2003",
+  },
+  {
+    quotation:
+      "Today, I consider myself the luckiest man on the face of the earth.",
+    author: "The Pride of the Yankees,1942",
+  },
+  {
+    quotation: "You is kind. You is smart. You is important.",
+    author: "The Help,2011",
+  },
+  {
+    quotation: "What we've got here is failure to communicate.",
+    author: "Cool Hand Luke,1967",
+  },
+  {
+    quotation: "Hasta la vista, baby.",
+    author: "Terminator2: Judgment Day,1991",
+  },
+  {
+    quotation:
+      "You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.",
+    author: "On the Waterfront,1954",
+  },
+  {
+    quotation: "Bond. James Bond.",
+    author: "Dr. No,1962",
+  },
+  {
+    quotation: "You talking to me?",
+    author: "Taxi Driver,1976",
+  },
+  {
+    quotation: "Roads? Where we're going we don't need roads.",
+    author: "Back to the Future,1985",
+  },
+  {
+    quotation: "That'll do, pig. That'll do.",
+    author: "Babe,1995",
+  },
+  {
+    quotation: "I'm walking here! I'm walking here!",
+    author: "Midnight Cowboy,1969",
+  },
+  {
+    quotation: "It was beauty killed the beast.",
+    author: "King Kong,1933",
+  },
+  {
+    quotation: "Stella! Hey, Stella!",
+    author: "A Streetcar Named Desire,1951",
+  },
+  {
+    quotation: "As if!",
+    author: "Clueless,1995",
+  },
+  {
+    quotation: "Here's Johnny!",
+    author: "The Shining,1980",
+  },
+  {
+    quotation: "Rosebud.",
+    author: "Citizen Kane,1941",
+  },
+  {
+    quotation: "I'll have what she's having.",
+    author: "When Harry Met Sally,1989",
+  },
+  {
+    quotation: "Inconceivable!",
+    author: "The Princess Bride,1987",
+  },
+  {
+    quotation: "All right, Mr. DeMille, I'm ready for my close-up.",
+    author: "Sunset Boulevard,1950",
+  },
+  {
+    quotation: "Fasten your seatbelts. It's going to be a bumpy night.",
+    author: "All About Eve,1950",
+  },
+  {
+    quotation: "Nobody puts Baby in a corner.",
+    author: "Dirty Dancing,1987",
+  },
+  {
+    quotation: "Well, nobody's perfect.",
+    author: "Some Like it Hot,1959",
+  },
+  {
+    quotation: "Snap out of it!",
+    author: "Moonstruck,1987",
+  },
+  {
+    quotation: "You had me at ‘hello.’",
+    author: "Jerry Maguire,1996",
+  },
+  {
+    quotation: "They may take our lives, but they'll never take our freedom!",
+    author: "Braveheart,1995",
+  },
+  {
+    quotation: "To infinity and beyond!",
+    author: "Toy Story,1995",
+  },
+  {
+    quotation: "You’re killin’ me, Smalls.",
+    author: "The Sandlot,1993",
+  },
+  {
+    quotation: "Toto, I've a feeling we're not in Kansas anymore.",
+    author: "The Wizard of Oz,1939",
+  },
+];
+
+export default movieQuotes;
--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -1,27 +1,33 @@
 import React, { useState } from "react";
 import TwitterImg from "../images/Twitter_Social_Icon_Square_White.png";
+import MovieQuotes from "../assets/movie_quotes";
 
 function Wrapper() {
-  const [quotation, setQuotation] = useState(
+  const [quote, setQuote] = useState(
     "Inspiration moves one from Pessimism to Optimism, from Doubt to Faith, from Despair to Hope, and from Darkness to Light!"
   );
-  const [author, setCharacter] = useState("author");
+  const [author, setAuthor] = useState("author");
 
   const handleClick = () => {
-    const proxy = "https://cors-anywhere.herokuapp.com/";
-    const api = `${proxy}https://thesimpsonsquoteapi.glitch.me/quotes?count=50`;
+    // since api is slow and unstable, I'm commenting this feature out and adding the quotes db myself.
+    // I'll leave the code snippets though
+    // const proxy = "https://cors-anywhere.herokuapp.com/";
+    // const api = `${proxy}https://thesimpsonsquoteapi.glitch.me/quotes?count=50`;
+    // fetch(api)
+    //   .then((request) => request.json())
+    //   .then((data) => {
+    //     setQuotation(data[0].quote);
+    //     setCharacter(data[0].character);
+    //   });
 
-    fetch(api)
-      .then((request) => request.json())
-      .then((data) => {
-        setQuotation(data[0].quote);
-        setCharacter(data[0].character);
-      });
+    let getRandomQuote = Math.floor(Math.random() * MovieQuotes.length);
+    setQuote(MovieQuotes[getRandomQuote].quotation);
+    setAuthor(MovieQuotes[getRandomQuote].author);
   };
 
   return (
     <div id="quote-box" className="flip-card">
-      <h3 id="text">{quotation}</h3>
+      <h3 id="text">{quote}</h3>
       <p id="author">{author}</p>
       <a href="https://twitter.com/intent/tweet" id="tweet-quote">
         <img
