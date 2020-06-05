8d10ec69149a022859240bda708637cb69e02da0
Mollie-S <olcha.olcha@gmail.com>
2020-06-05 17:21:28 +0200

    Updates

---
+++ precache-manifest.77bc0dd0ba8cae2c5c4f6b1019f15533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61f49ccf52e8b68969df6730e8d5823",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "0c99704b2b778a1bafaf",
+    "url": "/random-quote-machine/static/css/main.c4eeaa6a.chunk.css"
+  },
+  {
+    "revision": "b944f615ac4968d69a69",
+    "url": "/random-quote-machine/static/js/2.4ca01679.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.4ca01679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c99704b2b778a1bafaf",
+    "url": "/random-quote-machine/static/js/main.27fd383b.chunk.js"
+  },
+  {
+    "revision": "2cec414998c69e0a692f",
+    "url": "/random-quote-machine/static/js/runtime-main.5e7c1c70.js"
+  },
+  {
+    "revision": "7d995a6c87d5c9ee8d8469d686824085",
+    "url": "/random-quote-machine/static/media/eberhard-grossgasteiger-KLSvEQcMdT0-unsplash.7d995a6c.jpg"
+  }
+]);
